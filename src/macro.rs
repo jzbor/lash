490bab8626fe044cb5499ad5a703c7bb60714b09
jzbor <jzbor@posteo.net>
2023-04-23 21:48:31 +0200

    Improve macro parsing

--- src/macro.rs
+++ src/macro.rs
@@ -1,54 +1,42 @@
 use std::fmt::Display;
 
+use clap::ArgEnum;
+
 use crate::{lambda::*, interpreter::Interpreter};
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone, Copy, clap::ValueEnum)]
+#[clap(rename_all = "lower")]
 pub enum Macro {
     Debug,
     Nop,
+    NewLine,
     Normalize,
-    NormalizeVerbose,
+    VNormalize,
     Reduce,
-    ReduceVerbose,
+    VReduce,
 }
 
 impl Macro {
     pub fn get(name: &str) -> Option<Self> {
-        match name {
-            "debug" => Some(Self::Debug),
-            "nop" => Some(Self::Nop),
-            "normalize" => Some(Self::Normalize),
-            "reduce" => Some(Self::Reduce),
-            "vnormalize" => Some(Self::NormalizeVerbose),
-            "vreduce" => Some(Self::ReduceVerbose),
-            _ => None,
-        }
+        clap::ValueEnum::from_str(name, false).ok()
     }
 
     pub fn apply(self, interpreter: &Interpreter, term: LambdaTree) -> LambdaTree {
         use Macro::*;
         match self {
             Debug => { println!("{}", term); term },
+            NewLine => { println!(); term },
             Nop => term,
             Normalize => interpreter.strategy().normalize(term.clone(), false),
-            NormalizeVerbose => interpreter.strategy().normalize(term.clone(), true),
+            VNormalize => interpreter.strategy().normalize(term.clone(), true),
             Reduce => if let Some(reduced) = interpreter.strategy().reduce(term.clone(), false) { reduced } else { term },
-            ReduceVerbose => if let Some(reduced) = interpreter.strategy().reduce(term.clone(), true) { reduced } else { term },
+            VReduce => if let Some(reduced) = interpreter.strategy().reduce(term.clone(), true) { reduced } else { term },
         }
     }
 }
 
 impl Display for Macro {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        use Macro::*;
-        let s = match self {
-            Debug => "debug",
-            Nop => "nop",
-            Normalize => "normalize",
-            Reduce => "reduce",
-            NormalizeVerbose => "vnormalize",
-            ReduceVerbose => "vreduce",
-        };
-        write!(f, "{}", s)
+        write!(f, "{}", self.to_possible_value().unwrap().get_name())
     }
 }
